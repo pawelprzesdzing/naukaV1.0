246b328f2c485f8b07f5483ca52557fb7fcda090
pawel.przesdzing <pawel.przesdzing@gmail.com>
2019-08-09 09:58:36 +0200

    gettery i settery

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -7,4 +7,44 @@ public class SecondClass {
     private String thirdString;
     private String fourthString;
     private int first;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String getSecondString() {
+        return secondString;
+    }
+
+    public void setSecondString(String secondString) {
+        this.secondString = secondString;
+    }
+
+    public String getThirdString() {
+        return thirdString;
+    }
+
+    public void setThirdString(String thirdString) {
+        this.thirdString = thirdString;
+    }
+
+    public String getFourthString() {
+        return fourthString;
+    }
+
+    public void setFourthString(String fourthString) {
+        this.fourthString = fourthString;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
 }
